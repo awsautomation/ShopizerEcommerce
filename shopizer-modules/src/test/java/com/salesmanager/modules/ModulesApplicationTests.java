ea60567165eae727ba121da0c52ef27bdff2c7bd
shopizer-ecommerce <csamson777@yahoo.com>
2019-04-17 20:29:52 -0400

    Version 3.0.0

---
+++ shopizer-modules/src/test/java/com/salesmanager/modules/ModulesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.salesmanager.modules;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ModulesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
